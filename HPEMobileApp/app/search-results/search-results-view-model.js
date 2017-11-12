7d566405166bfd124ca681c061f0c31339f61657
joshualevine <32550989+joshualevine@users.noreply.github.com>
2017-11-12 18:12:24 -0500

    Add files via upload
    search page format set up

--- HPEMobileApp/app/search-results/search-results-view-model.js
+++ HPEMobileApp/app/search-results/search-results-view-model.js
@@ -1,6 +1,6 @@
 const observableModule = require("data/observable");
 
-function SearchViewModel() {
+function SearchResultsViewModel() {
     const viewModel = observableModule.fromObject({
 
     });
@@ -8,4 +8,4 @@ function SearchViewModel() {
     return viewModel;
 }
 
-module.exports = SearchViewModel;
+module.exports = SearchResultsViewModel;
