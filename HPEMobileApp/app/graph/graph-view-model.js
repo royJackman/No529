8cade72907ec1292e9523af26af34fe8baa89059
Zain Aziz <zsaziz@umass.edu>
2017-11-17 12:31:22 -0500

    Create graph-view-model.js

---
+++ HPEMobileApp/app/graph/graph-view-model.js
@@ -0,0 +1,11 @@
+const observableModule = require("data/observable");
+
+function GraphViewModel() {
+    const viewModel = observableModule.fromObject({
+
+    });
+
+    return viewModel;
+}
+
+module.exports = GraphViewModel;
