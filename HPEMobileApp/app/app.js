fada5b4a4474bec63be58fb19492f8171cdf478c
Alfonso Merkl <anmerkl@gmail.com>
2017-11-20 21:12:08 -0500

    Login page (#13)
    * edited login page to actually work
    
    * removing redundant files

--- HPEMobileApp/app/app.js
+++ HPEMobileApp/app/app.js
@@ -1,7 +1,7 @@
 require("./bundle-config");
 const application = require("application");
 
-application.start({ moduleName: "home/home-page" });
+application.start({ moduleName: "login/login" });
 
 /*
 Do not place any code after the application has been started as it will not
