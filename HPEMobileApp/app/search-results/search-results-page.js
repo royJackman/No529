988be4c15379f401f0aee0e1864869693d476518
Josh <joshsam925@gmail.com>
2017-12-13 16:19:01 -0500

    Added ability to talk to server and display results. Some bugs still.

--- HPEMobileApp/app/search-results/search-results-page.js
+++ HPEMobileApp/app/search-results/search-results-page.js
@@ -2,11 +2,15 @@ const frameModule = require("ui/frame");
 
 const SearchResultsViewModel = require("./search-results-view-model");
 
+const SearchResultsListViewModel = require("./search-results-list-view-model");
+
 var fetchModule = require("fetch");
 /* ***********************************************************
 * Use the "onNavigatingTo" handler to initialize the page binding context.
 *************************************************************/
 var searchResult = new SearchResultsViewModel();
+
+var searchResultList = new SearchResultsListViewModel();
 
 function onNavigatingTo(args) {
     /* ***********************************************************
@@ -18,11 +22,29 @@ function onNavigatingTo(args) {
         return;
     }
 
-    searchResult.searchFields = args.context;
-    buildhttpRequest();
+    searchResult.list = searchResultList;
 
-    const page = args.object;
+    const page = args.object;    
     page.bindingContext = searchResult;
+
+    //This checks if we are returning from viewing a system's info. We want to display what we already found.
+    if(args.context == "back") return;
+
+    searchResult.searchFields = args.context;
+    var oldhttp = searchResult.httpRequest;
+    searchResult.clearRequest();
+    buildhttpRequest();
+    //This makes sure we don't send the same request multiple times in a row.
+    if(oldhttp != searchResult.httpRequest){
+        searchResult.set("isLoading", true);
+        searchResult.clearResults();
+        searchResult.getSearchResults().then(function(){
+            searchResult.set("isLoading", false);
+            if(searchResult.results == 404){
+                alert({ title: "Cannot connect to server", message: searchResult.results, okButtonText: "Close" });
+            }
+        });
+    }
 }
 
 /* ***********************************************************
@@ -48,23 +70,43 @@ function editSearch(){
     frameModule.topmost().navigate(navigationEntry);
 }
 
-function getSearchResults() {
-    fetchModule.fetch(searchResult.httpRequest, {
-        method: "GET"
-    })
-    .then(
-        function (response) {
-            searchResult.resultsJSON = response;
-            //alert({ title: "GET Response", message: JSON.stringify(response), okButtonText: "Close" });
-        }, 
-        function (error) {
-            console.log(JSON.stringify(error));
-        }
-    )
-}
+// function getSearchResults() {
+//     console.log("got here 0\n");
+//     fetchModule.fetch(searchResult.httpRequest, {
+//         method: "GET"
+//     }).then(
+//         function (res) {
+//             //console.dir(res);
+//             //console.log("got here 1\n");
+//             searchResult.results = JSON.parse(res._bodyText);
+//             //console.log("got here 2\n");
+//             //console.dir(searchResult.results);
+//             //alert({ title: "GET Response", message: JSON.stringify(response), okButtonText: "Close" });
+//             searchResult.results.forEach(function(sys) {
+//                 searchResultList.push({
+//                     system: sys,
+//                     detailedView: function(){
+//                         var navigationEntry = {
+//                             moduleName: "configuration/configuration-page",
+//                             context: this.system,
+//                             animated: false
+//                         };
+//                         frameModule.topmost().navigate(navigationEntry);
+//                     }
+//                 });
+//             });
+//             searchResult.set("isLoading", false);
+//         }, 
+//         function (error) {
+//             console.log(JSON.stringify(error));
+//         }
+//     )
+// }
 
+//Builds http request for given search request
 function buildhttpRequest() {
     var first = true;
+    searchResult.httpRequest += "/";
     for (var key in searchResult.searchFields){
         if(searchResult.searchFields.hasOwnProperty(key) && searchResult.searchFields[key] != null){
             if(first){
--- HPEMobileApp/app/search-results/search-results-view-model.js
+++ HPEMobileApp/app/search-results/search-results-view-model.js
@@ -1,14 +1,40 @@
 const observableModule = require("data/observable");
 
-//const searchViewModel = require("./../search/search-view-model");
+var fetchModule = require("fetch");
 
 function SearchResultsViewModel() {
     const viewModel = observableModule.fromObject({
         searchFields: null,
-        httpRequest: "https://nqexnakqdo.localtunnel.me/systems/Frenzy",
-        resultsJSON: null,
-        results: null
+        httpRequest: "https://hpbackend.localtunnel.me/systems/Frenzy",
+        results: null,
+        list: null
     });
+
+    viewModel.clearResults = function() {
+        viewModel.results = null;
+        viewModel.list.empty();
+    };
+
+    viewModel.clearRequest = function(){
+        viewModel.httpRequest = "https://hpbackend.localtunnel.me/systems/Frenzy";
+    }
+
+    viewModel.getSearchResults = function(){
+        return fetchModule.fetch(viewModel.httpRequest, {
+            method: "GET"
+        }).then(
+            function (res) {
+                viewModel.results = JSON.parse(res._bodyText);
+                console.log(viewModel.results);
+                if(viewModel.results != 404){
+                    viewModel.list.fill(viewModel.results);
+                }
+            }, 
+            function (error) {
+                console.log(JSON.stringify(error));
+            }
+        )
+    }
 
     return viewModel;
 }
