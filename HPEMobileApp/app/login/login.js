22bd2fed415c3a678e4c43bb4821d0a31e260dda
Alfonso Merkl <anmerkl@gmail.com>
2017-12-14 15:15:15 -0500

    started using backend for suer authentication

--- HPEMobileApp/app/login/login.js
+++ HPEMobileApp/app/login/login.js
@@ -1,19 +1,12 @@
 const frameModule = require("ui/frame");
 const LoginViewModel = require("./login-view-model");
+var fetchModule = require("fetch");
 
 var page;
 var email;
 var password;
 
-/* ***********************************************************
-* Use the "onNavigatingTo" handler to initialize the page binding context.
-*************************************************************/
 function onNavigatingTo(args) {
-    /* ***********************************************************
-    * The "onNavigatingTo" event handler lets you detect if the user navigated with a back button.
-    * Skipping the re-initialization on back navigation means the user will see the
-    * page in the same data state that he left it in before navigating.
-    *************************************************************/
     if (args.isBackNavigation) {
         return;
     }
@@ -22,26 +15,45 @@ function onNavigatingTo(args) {
     page.bindingContext = new LoginViewModel();
 }
 
-/* ***********************************************************
- * According to guidelines, if you have a drawer on your page, you should always
- * have a button that opens it. Get a reference to the RadSideDrawer view and
- * use the showDrawer() function to open the app drawer section.
- *************************************************************/
 function onDrawerButtonTap(args) {
     const sideDrawer = frameModule.topmost().getViewById("sideDrawer");
     sideDrawer.showDrawer();
 }
 
-exports.onNavigatingTo = onNavigatingTo;
-exports.onDrawerButtonTap = onDrawerButtonTap;
+exports.login = function() {
+  email = page.getViewById('email');
+  pword = page.getViewById('password');
+
+  var topmost = frameModule.topmost();
+  fetchModule.fetch(global.serverLink, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username: viewModel.get("email"),
+      password: viewModel.get("password")
+    })
+  })
+  .then(function(response) {
+    var confirmation = response.json();
+    if confirmation.status == 'valid' {
+      global.companyName = confirmation.company;
+      global.queryStart = global.serverLink + "/systems/" + global.companyName
+      topmost.navigate('home/home-page');
+    } else {
+      alert({
+        title: "Invalid Credentials",
+        message: "Incorrect Username or Password. Please try again.",
+        okButtonText: "Close"
+      });
+    }
+  })
+};
 
 exports.loaded = function(args) {
     page = args.object;
 };
 
-exports.login = function() {
-    email = page.getViewById('email');
-    console.log(email.text)
-    var topmost = frameModule.topmost();
-    topmost.navigate('home/home-page');
-};
+exports.onNavigatingTo = onNavigatingTo;
+exports.onDrawerButtonTap = onDrawerButtonTap;
