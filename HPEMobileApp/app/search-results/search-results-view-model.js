74bf3468b57b3de5efece0d544ccb2eb72ff86f9
Josh <joshsam925@gmail.com>
2017-12-14 15:54:28 -0500

    Added connection time out alert

--- HPEMobileApp/app/search-results/search-results-view-model.js
+++ HPEMobileApp/app/search-results/search-results-view-model.js
@@ -24,10 +24,14 @@ function SearchResultsViewModel() {
             method: "GET"
         }).then(
             function (res) {
-                console.log("Recieved response from server");                
+                console.log("Recieved response from server");
+                if(res.status == 504){
+                    alert({ title: "Cannot connect to server", message: "Connection timed out", okButtonText: "Close" });
+                    return;
+                }
                 viewModel.results = JSON.parse(res._bodyText);
                 if(viewModel.results == "404"){
-                    alert({ title: "Cannot connect to server", okButtonText: "Close" });
+                    alert({ title: "Cannot connect to server", message: "404 error", okButtonText: "Close" });
                     viewModel.clearRequest();
                 }else{
                     viewModel.list.fill(viewModel.results);
